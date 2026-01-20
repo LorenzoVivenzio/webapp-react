298fb9d40d0ca53acc5b5840d709cacd5094cf53
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-20 17:36:34 +0100

    feat: milestone 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/movies")
+            .then((resp) => {
+                setMovies(resp.data.results)
+                console.log(resp)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
     return (
         <>
-        <h1>Home</h1>
+            <h3 className="text-center mt-4">Lista dei film</h3>
+            <ul>
+                {
+                    movies.map((m) => {
+                        return <li key={m.id}>
+                            {m.title}
+                        </li>
+                    })
+                }
+            </ul>
         </>
     )
 }
