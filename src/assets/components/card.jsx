f8632a62e37dea9350e1224e081c67724a1f4db8
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-21 09:02:34 +0100

    feat: props della card

--- src/assets/components/card.jsx
+++ src/assets/components/card.jsx
@@ -1,12 +1,13 @@
-export default function card(props) {
-    // return (
-    //     <div className="card">
-    //         <img src={`http://localhost:3000/images/${props.image}`} className="card-img-top" alt={props.title} />
-    //         <div className="card-body">
-    //             <h5 className="card-title">{props.title}</h5>
-    //             <p className="card-text">Anno di rilascio: {props.release_year}</p>
-    //             <a href="#" className="btn btn-primary">vedi dettagli</a>
-    //         </div>
-    //     </div>
-    // )
+export default function Card(props) {
+    
+    return (
+        <div className="card">
+            <img src={`http://localhost:3000/images/${props.image}`} className="card-img-top" alt={props.title} />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">Anno di rilascio: {props.release_year}</p>
+                <a href="#" className="btn btn-primary">vedi dettagli</a>
+            </div>
+        </div>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import Card from "../assets/components/Card"
 
 export default function Home() {
 
@@ -25,16 +26,25 @@ export default function Home() {
 
             {
                 movies.map((m) => (
-                <div key={m.id} className="col">
-                    <div className="card">
-                        <img src= {`http://localhost:3000/images/${m.image}`} className="card-img-top" alt={m.title}/>
-                            <div className="card-body">
-                                <h5 className="card-title">{m.title}</h5>
-                                <p className="card-text">Anno di rilascio: {m.release_year}</p>
-                                <a href="#" className="btn btn-primary">vedi dettagli</a>
-                            </div>
-                    </div>
-                    </div>
+                    <div key={m.id} className="col">
+                    <Card 
+                    key={m.id}
+                    image={m.image}
+                    title={m.title}
+                    release_year={m.release_year}
+                    />
+
+                {/* // <div key={m.id} className="col">
+                //     <div className="card">
+                //         <img src= {`http://localhost:3000/images/${m.image}`} className="card-img-top" alt={m.title}/>
+                //             <div className="card-body">
+                //                 <h5 className="card-title">{m.title}</h5>
+                //                 <p className="card-text">Anno di rilascio: {m.release_year}</p>
+                //                 <a href="#" className="btn btn-primary">vedi dettagli</a>
+                //             </div>
+                //     </div>
+                //     </div> */}
+                </div>
                 ))
             }
                     
